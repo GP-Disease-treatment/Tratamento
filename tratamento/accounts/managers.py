069f9ec39a7f469200ab89191656094d5e1de23a
Renato Santos <rena02to@gmail.com>
2024-10-22 17:05:53 -0300

    fix: Correção na criação de superusuario e na apresentação dos dados no django admin

--- tratamento/accounts/managers.py
+++ tratamento/accounts/managers.py
@@ -11,9 +11,12 @@ class UserManager(BaseUserManager):
     def create_superuser(self, CPF, password, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_superuser", True)
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
         if extra_fields.get("is_active") is not True:
             raise ValueError(_("Superuser must have is_active=True."))
         return self.create_user(CPF, password, **extra_fields)
