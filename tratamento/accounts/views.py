7657ffa4b1fa5e71a9ed4b9833b7a20f8857cd66
Filipe Mota <fsmota@ic.ufal.br>
2024-10-31 17:32:54 -0300

    :recycle: refactor: adjusting the views and urls

--- tratamento/accounts/views.py
+++ tratamento/accounts/views.py
@@ -7,26 +7,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
 import google.generativeai as genai
 
-class LoginView(APIView):
-    permission_classes = [AllowAny]
-    def post(self, request):
-        cpf = request.data.get('CPF')
-        password = request.data.get('password')
-
-        user = authenticate(request, CPF=cpf, password=password)
-        if user is not None:
-            type = User.objects.values('type').get(CPF=cpf).get('type')
-            if type != 'Captador':
-                token = RefreshToken.for_user(user).access_token
-                return Response({'message': 'Authenticated successfully', 'token': f'{token}', 'type': type}, status=status.HTTP_200_OK)
-            else:
-                return Response({'error:': 'User without permission to access system.'}, status=status.HTTP_401_UNAUTHORIZED)
-        else:
-            return Response({'message': 'There is no user with these access credentials'}, status=status.HTTP_404_NOT_FOUND)
-
-
-
-class GPTView(APIView):
+class TratamentoView(APIView):
     def post(self, request):
         data = request.data.get('prognostico')
         genai.configure()
