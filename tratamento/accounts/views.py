16ed4152d7ec4403918118dd7147a740e5109403
Filipe Mota <fsmota@ic.ufal.br>
2024-11-04 10:06:21 -0300

    :recycle: refactore: promp refine

--- tratamento/accounts/views.py
+++ tratamento/accounts/views.py
@@ -10,7 +10,9 @@ import google.generativeai as genai
 class TratamentoView(APIView):
     def post(self, request):
         data = request.data.get('prognostico')
+        pront = request.data.get('prontuario')
         genai.configure()
         model = genai.GenerativeModel('gemini-pro')
-        response = model.generate_content("Baseado no seguinte prognóstico e condição de saúde do usuário relacionada ao fígado, por favor, indique as melhores opções de tratamento e recomendações médicas detalhadas. Prognóstico do usuário:"+data)
+        response = model.generate_content("""Baseado no seguinte prognóstico e condição de saúde do usuário relacionada aDado o prognostico:"""+data+""" e o seguinte prontuário de paciente:"""+pront+""". Gere um tratamento adequado e separe em parágrafos da seguinte forma: principal tratamento, medicamentos e por fim recomendações para recuperação.
+Por fim um parágrafo breve agradecendo o paciente e desejando melhoraso fígado, por favor, indique as melhores opções de tratamento e recomendações médicas detalhadas . Prognóstico do usuário:""")
         return Response({'tratamento': response.text},status=status.HTTP_200_OK)
